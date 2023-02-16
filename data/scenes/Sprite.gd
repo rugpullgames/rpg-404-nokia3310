ab1980b1437c3aeb1a37ade5ae6c2ea1e781e037
dewolen <dewolen@gmail.com>
2023-02-16 22:26:36 +0100

    Add instructions
    Added instructions and explanations inside a label in the Main scene. Changed some project settings. Added default InputMap actions.

--- data/scenes/Sprite.gd
+++ data/scenes/Sprite.gd
@@ -21,7 +21,7 @@ func _process(delta: float) -> void:
 
 
 func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("jump"):
+	if event is InputEventKey and event.scancode == KEY_SPACE and event.pressed:
 		if _position == Vector2.ZERO:
 			AudioManager.play_sfx(sfx)
 			_velocity.y = 30
